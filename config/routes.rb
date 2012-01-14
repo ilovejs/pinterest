12b8e53dfa4559d993e539b2fb05089a1cfbac43
jokry <wenglonghui@gmail.com>
2012-01-14 23:54:45 +0800

    Done with categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinterest::Application.routes.draw do
+  resources :categories
+
   devise_for :users, :path_names => { :sign_up => "register" , :sign_in => "login"}    
 
   root :to => "home#index"
